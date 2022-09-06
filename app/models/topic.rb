eff05a2e4de2ca719655f06e3ba00973873431f7
SpyrexDE <malikfk100@gmail.com>
2022-09-06 10:26:27 +0200

    Add Hotwire

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  after_create_commit { broadcast_prepend_to "topics"}
+  after_update_commit { broadcast_replace_to "topics"}
+  after_destroy_commit { broadcast_remove_to "topics"}
 end
