2566d93f3e037853bb97e7d3d270f4d49e5aa5a7
Spyrex <malikfk100@gmail.com>
2023-03-10 12:29:52 +0100

    Fix user icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  APPLICATION_TITLE = "mooted.org"
+  APPLICATION_TITLE = "Polymo.org"
   add_flash_types :success, :info, :error, :warning
 end
